a378bbd16ef1a7c75cf219d1e3355cd54d374aa8
Fabiano-Barbosa <62999333+Fabiano-Barbosa@users.noreply.github.com>
2021-02-08 01:48:27 -0300

    base pronta

--- linx_previsao.py
+++ linx_previsao.py
@@ -37,7 +37,7 @@ def previsao():
           prev_c = []
           prev_h = []
           json_prev    = previsao.json()
-          for i in range(0,12,+2):
+          for i in range(0,40,+2):
             temp_k  = [float(json_prev['list'][i]['main']['temp_min']), float(json_prev['list'][i]['main']['temp_max'])]
             prev_c.append( [round((temp_k[0] - 273.15),1), round((temp_k[1] - 273.15),1)] )
             prev_h.append ( datetime.datetime.fromtimestamp( int(json_prev['list'][i]['dt']) ).strftime('%d/%m/%Y %H:%M') )
@@ -66,8 +66,27 @@ def previsao():
                                  prev_hora_0=prev_h[0], prev_0_1=prev_c[0][0], prev_0_2=prev_c[0][1],
                                  prev_hora_1=prev_h[1], prev_1_1=prev_c[1][0], prev_1_2=prev_c[1][1],
                                  prev_hora_2=prev_h[2], prev_2_1=prev_c[2][0], prev_2_2=prev_c[2][1],
+                                 prev_hora_3=prev_h[3], prev_3_1=prev_c[3][0], prev_3_2=prev_c[3][1],
+
                                  prev_hora_4=prev_h[4], prev_4_1=prev_c[4][0], prev_4_2=prev_c[4][1],
-                                 prev_hora_3=prev_h[3], prev_3_1=prev_c[3][0], prev_3_2=prev_c[3][1]
+                                 prev_hora_5=prev_h[5], prev_5_1=prev_c[5][0], prev_5_2=prev_c[5][1],
+                                 prev_hora_6=prev_h[6], prev_6_1=prev_c[6][0], prev_6_2=prev_c[6][1],
+                                 prev_hora_7=prev_h[7], prev_7_1=prev_c[7][0], prev_7_2=prev_c[7][1],
+
+                                 prev_hora_8=prev_h[8], prev_8_1=prev_c[8][0], prev_8_2=prev_c[8][1],
+                                 prev_hora_9=prev_h[9], prev_9_1=prev_c[9][0], prev_9_2=prev_c[9][1],
+                                 prev_hora_10=prev_h[10], prev_10_1=prev_c[10][0], prev_10_2=prev_c[10][1],
+                                 prev_hora_11=prev_h[11], prev_11_1=prev_c[11][0], prev_11_2=prev_c[11][1],
+
+                                 prev_hora_12=prev_h[12], prev_12_1=prev_c[12][0], prev_12_2=prev_c[12][1],
+                                 prev_hora_13=prev_h[13], prev_13_1=prev_c[13][0], prev_13_2=prev_c[13][1],
+                                 prev_hora_14=prev_h[14], prev_14_1=prev_c[14][0], prev_14_2=prev_c[14][1],
+                                 prev_hora_15=prev_h[15], prev_15_1=prev_c[15][0], prev_15_2=prev_c[15][1],
+
+                                 prev_hora_16=prev_h[16], prev_16_1=prev_c[16][0], prev_16_2=prev_c[16][1],
+                                 prev_hora_17=prev_h[17], prev_17_1=prev_c[17][0], prev_17_2=prev_c[17][1],
+                                 prev_hora_18=prev_h[18], prev_18_1=prev_c[18][0], prev_18_2=prev_c[18][1],
+                                 prev_hora_19=prev_h[19], prev_19_1=prev_c[19][0], prev_19_2=prev_c[19][1]
                                  )
 
 @app.route('/log', methods=['GET'])
